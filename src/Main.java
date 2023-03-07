5ff71976359a8fc1ab4bd1f3c9aa7b813a4578d3
Elena Zh <vermilion7448@gmail.com>
2023-03-07 20:44:03 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int clientCash = 500;
-        int addCash = 3300;
+        int clientCash = 100;
+        int addCash = 1100;
         int bonus = addCash / 100;
-        int finalCash = clientCash + addCash + bonus;
+        //int finalCash = clientCash + addCash + bonus;
 
 
         if (addCash > 1000) {
 
-            System.out.println(finalCash);
+            System.out.println(clientCash + addCash + bonus);
             System.out.println(bonus);
 
         } else {
-            System.out.println(finalCash);
+            System.out.println(clientCash + addCash);
             System.out.println(bonus = 0);
         }
     }
